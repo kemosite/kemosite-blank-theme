9ed810ca03e6156908dfd06450e20e9603cb5b7b
Kevin Montgomery <github_kevin@kemosite.com>
2019-01-07 00:44:55 -0500

    Update to README and theme dashboard.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-
 # kemosite-blank-theme
 Blank Wordpress Theme 4.9.8.1
 
 Contributors: Kevin Montgomery
-Tags: adaptive, javascript
+Tags: adaptive, javascript, responsive, foundation, less, woocommerce, github
 Requires at least: 4.0
 Tested up to: 4.9.8
 License: GNU General Public License v2 or later
@@ -12,20 +11,16 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Kevin Montgomery's Blank Wordpress Theme. Child of default Wordpress theme. Version to match version of WordPress supported.
 
 ## Features
-
-
+ - Foundation 6.4.2
+ - Less 2.7.2
+ - Woocommerce
+ - Github Updater
+ - Incorporates QODE Central Wordpress theme shortcodes
 
 ## Installation
+Method 1 - Use Github Updater
+Theme URI: https://github.com/kemosite/kemosite-blank-theme
 
-Method 1: (GIT in Wordpress)
-
-Method 2:
-Download ZIP file. Place into "themes" folder. Activate in WordPress.
-
-## Screenshots
-
- 1. Screenshot #1
- 2. Screenshot #2
- 3. Screenshot #3
-
-## Changelog
+Method 2 Download ZIP file
+Download from Github repository: https://github.com/kemosite/kemosite-blank-theme
+Decompress. Place into "themes" folder. Activate in WordPress.
--- functions-dashboard-setup.php
+++ functions-dashboard-setup.php
@@ -25,20 +25,7 @@ function kemosite_blank_nextsteps_widget_display() {
     ?>
     <h1>Security</h1>
     <h3><strong>wp-config.php</strong></h3>
-    <p>To deny access to <em>wp-config.php</em>, you should add the code below at the top of the <em>.htaccess</em> file:</p>
-    <pre>
-    &lt;files wp-config.php&gt;
-    	order allow,deny
-    	deny from all
-    &lt;/files&gt;
-	</pre>
-	<p>This file shouldn't be modifiable or writable by others. To prevent other users from reading it, the file permission should be 440 or 400.</p>
 	<p>If you ever need to regenerate the secret keys, you can visit the official generator provided by WordPress.org at <a href="https://api.wordpress.org/secret-key/1.1/salt/">https://api.wordpress.org/secret-key/1.1/salt/</a>.</p>
-	<h3><strong>File Server</strong></h3>
-	<p>Ensuring that no one can view the contents of directories can be done by adding a single line in <em>.htaccess</em></p>
-	<pre>
-	Options –Indexes
-	</pre>
 	<h1>Plug-Ins</h1>
 	<h3><strong>Required Plugins</strong></h3>
 	<p>kemosite typography plug-in for Wordpress<br>
@@ -46,36 +33,11 @@ function kemosite_blank_nextsteps_widget_display() {
 	/* [DETECT KEMOSITE TYPOGRAPHY PLUGIN] */
 	if ( is_plugin_active( 'kemosite-typography-plugin/index.php' ) ): ?><em>Active</em><?php else: ?><em>Inactive</em><?php endif; ?>
 	</p>
-    <p>Install these plugins, if functionality needed.</p>
-    <ol>
-    <li>Jetpack</li>
-	<li>Akismet</li>
-	<li>iThemes Security</li>
-	<li>Exploit Scanner</li>
-	<li>Quttera Web Malware Scanner</li>
-	<li>Duplicator</li>
-	<li>Yoast SEO</li>
-	<li>All in One SEO Pack</li>
-	<li>WP Performance Score Booster</li>
-	<li>WP Smush.it</li>
-	<li>WP Super Cache</li>
-	<li>WP Super Minify</li>
-	<li>WP-DBManager</li>
-	<li>Simple Facebook Page Plugin</li>
-	<li>Facebook Comments Plugin</li>
-	<li>Disqus Comment System</li>
-	<li>VaultPress</li>
-	<li>Regenerate Thumbnails</li>
-	<li>Contact Form DB</li>
-	<li>OptionTree</li>
-	<li>Scribe</li>
-	<li>MailPoet Newsletters</li>
-	<li>Google Consumer Surveys</li>
-	<li>Project Status</li>
-	<li>Events Manager</li>
-	<li>Advanced Ads</li>
-	<li>WooCommerce</li>
-    </ol>
+	<p>GitHub Updater<br>
+	<?php
+	/* [DETECT GITHUB UPDATER PLUGIN] */
+	if ( is_plugin_active( 'github-updater/github-updater.php' ) ): ?><em>Active</em><?php else: ?><em>Inactive</em><?php endif; ?>
+	</p>
     <?php
 }
 add_action( 'wp_dashboard_setup', 'register_my_dashboard_widget' );
